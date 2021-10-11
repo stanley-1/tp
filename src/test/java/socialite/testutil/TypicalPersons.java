b480b67a25bf4dd7848ca53f817d57e82935b0a8
Stanley Tan <stanley.tan@u.nus.edu>
2021-10-11 21:41:42 +0800

    Rectify checkstyle error in test code

--- src/test/java/socialite/testutil/TypicalPersons.java
+++ src/test/java/socialite/testutil/TypicalPersons.java
@@ -4,14 +4,14 @@ import static socialite.logic.commands.CommandTestUtil.VALID_ADDRESS_AMY;
 import static socialite.logic.commands.CommandTestUtil.VALID_ADDRESS_BOB;
 import static socialite.logic.commands.CommandTestUtil.VALID_EMAIL_AMY;
 import static socialite.logic.commands.CommandTestUtil.VALID_EMAIL_BOB;
+import static socialite.logic.commands.CommandTestUtil.VALID_FACEBOOK_AMY;
+import static socialite.logic.commands.CommandTestUtil.VALID_FACEBOOK_BOB;
 import static socialite.logic.commands.CommandTestUtil.VALID_NAME_AMY;
 import static socialite.logic.commands.CommandTestUtil.VALID_NAME_BOB;
 import static socialite.logic.commands.CommandTestUtil.VALID_PHONE_AMY;
 import static socialite.logic.commands.CommandTestUtil.VALID_PHONE_BOB;
 import static socialite.logic.commands.CommandTestUtil.VALID_TAG_FRIEND;
 import static socialite.logic.commands.CommandTestUtil.VALID_TAG_HUSBAND;
-import static socialite.logic.commands.CommandTestUtil.VALID_FACEBOOK_AMY;
-import static socialite.logic.commands.CommandTestUtil.VALID_FACEBOOK_BOB;
 
 import java.util.ArrayList;
 import java.util.Arrays;
