ac272247ee978960f6005bf4236b887d85410d7c
Stanley Tan <stanley.tan@u.nus.edu>
2021-10-12 16:45:52 +0800

    Add Facebook icon to UI

--- src/main/java/socialite/ui/PersonCard.java
+++ src/main/java/socialite/ui/PersonCard.java
@@ -4,6 +4,8 @@ import java.util.Comparator;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Region;
@@ -39,6 +41,8 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private Label email;
     @FXML
+    private ImageView facebookIcon;
+    @FXML
     private Label facebook;
     @FXML
     private FlowPane tags;
@@ -57,7 +61,8 @@ public class PersonCard extends UiPart<Region> {
         person.getTags().stream()
                 .sorted(Comparator.comparing(tag -> tag.tagName))
                 .forEach(tag -> tags.getChildren().add(new Label(tag.tagName)));
-        facebook.setText("Facebook: @" + person.getFacebook().value);
+        facebookIcon.setImage(new Image(this.getClass().getResourceAsStream("/images/facebook.png")));
+        facebook.setText(" @" + person.getFacebook().value);
     }
 
     @Override
