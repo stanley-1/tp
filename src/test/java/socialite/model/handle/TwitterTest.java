40cc67e00bcabd1821b21e3516caa3bd5ba202df
zacchua <zacchua@gmail.com>
2021-10-12 15:11:50 +0800

    Add some tests for valid twitter handle

---
+++ src/test/java/socialite/model/handle/TwitterTest.java
@@ -0,0 +1,43 @@
+package socialite.model.handle;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import socialite.testutil.Assert;
+
+public class TwitterTest {
+    @Test
+    public void constructor_null_throwsNullPointerException() {
+        Assert.assertThrows(NullPointerException.class, () -> new Twitter(null));
+    }
+
+    @Test
+    public void constructor_invalidEmail_throwsIllegalArgumentException() {
+        Assert.assertThrows(IllegalArgumentException.class, () -> new Twitter(""));
+    }
+
+    @Test
+    public void isValidHandle() {
+        // check null
+        Assert.assertThrows(NullPointerException.class, () -> Twitter.isValidHandle(null));
+
+        // check empty string and spaces
+        assertFalse(Twitter.isValidHandle(""));
+        assertFalse(Twitter.isValidHandle("  "));
+
+        // test invalid names, wrong lengths and non alphanumerical or '_'
+        assertFalse(Twitter.isValidHandle("bob"));
+        assertFalse(Twitter.isValidHandle("somethingverylong"));
+        assertFalse(Twitter.isValidHandle("bob.by"));
+        assertFalse(Twitter.isValidHandle("bob$%"));
+
+        // valid names
+        assertTrue(Twitter.isValidHandle("bobb"));
+        assertTrue(Twitter.isValidHandle("BOBBY"));
+        assertTrue(Twitter.isValidHandle("bobby_boy"));
+        assertTrue(Twitter.isValidHandle("bObbY_boy99"));
+        assertTrue(Twitter.isValidHandle("12345"));
+    }
+}
