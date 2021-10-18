9ff8368bc7af4b9c597ffb22cd4ce2a5d450b8f5
EssWhyy <swenyitang99@gmail.com>
2021-10-18 23:52:13 +0800

    Add ability for find to search for tags and platform handles
    Add reset keyword for find to allow for users to revert to full list after searching

--- src/main/java/socialite/logic/commands/FindCommand.java
+++ src/main/java/socialite/logic/commands/FindCommand.java
@@ -14,10 +14,13 @@ public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose names contain any of "
-            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n"
-            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
-            + "Example: " + COMMAND_WORD + " alice bob charlie";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Filters contacts based on specified keywords.\n"
+            + "Keywords can be in the form of tags (prefix with 't/'),\n"
+            + "handles (prefix with 'p/'), and names (no prefix needed).\n"
+            + "Returns all contacts containing ALL keywords listed in the command.\n"
+            + "Parameters: find [KEYWORDS]...\n"
+            + "Example: " + COMMAND_WORD + " alice t/colleagues p/instagram\n"
+            + "Type 'find all/' to reset to full list of contacts";
 
     private final NameContainsKeywordsPredicate predicate;
 
--- src/main/java/socialite/model/person/NameContainsKeywordsPredicate.java
+++ src/main/java/socialite/model/person/NameContainsKeywordsPredicate.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.function.Predicate;
 
 import socialite.commons.util.StringUtil;
+import socialite.model.tag.Tag;
 
 /**
  * Tests that a {@code Person}'s {@code Name} matches any of the keywords given.
@@ -15,10 +16,53 @@ public class NameContainsKeywordsPredicate implements Predicate<Person> {
         this.keywords = keywords;
     }
 
+    private boolean testName(Person person, String keyword) {
+        return StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword);
+    }
+
+    private boolean testTags(Person person, String keyword) {
+        for (Tag tag : person.getTags()){
+            if (StringUtil.containsWordIgnoreCase(tag.tagName, keyword)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean testPlatforms(Person person, String keyword) {
+
+        switch (keyword){
+        case "facebook":
+            return !person.getFacebook().value.equals("");
+        case "instagram":
+            return !person.getInstagram().value.equals("");
+        case "telegram":
+            return !person.getTelegram().value.equals("");
+        case "tiktok":
+            return !person.getTiktok().value.equals("");
+        case "twitter":
+            return !person.getTwitter().value.equals("");
+        default:
+            return false;
+        }
+
+    }
+
     @Override
     public boolean test(Person person) {
-        return keywords.stream()
-                .anyMatch(keyword -> StringUtil.containsWordIgnoreCase(person.getName().fullName, keyword));
+        //Tests just one person. For their name, tags or handles.
+        for (String keyword : keywords){
+            if (keyword.equals("all/")){
+                return true;
+            } else if(keyword.startsWith("t/")){
+                return testTags(person, keyword.substring(2));
+            } else if (keyword.startsWith("p/")){
+                return testPlatforms(person, keyword.substring(2));
+            } else{
+                return testName(person, keyword);
+            }
+        }
+        return true;
     }
 
     @Override
