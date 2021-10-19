19b30f3f9cea8013faf2415fcd3c749499353c35
Stanley Tan <stanley.tan@u.nus.edu>
2021-10-19 21:18:51 +0800

    Create additional tests for improved `HelpCommand`

--- src/test/java/socialite/logic/commands/HelpCommandTest.java
+++ src/test/java/socialite/logic/commands/HelpCommandTest.java
@@ -15,6 +15,42 @@ public class HelpCommandTest {
     public void execute_help_success() {
         CommandResult expectedCommandResult =
                 new CommandResult(HelpCommand.SHOWING_HELP_MESSAGE, true, false);
-        assertCommandSuccess(new HelpCommand(), model, expectedCommandResult, expectedModel);
+        assertCommandSuccess(new HelpCommand(HelpCommand.GENERIC_RESPONSE), model, expectedCommandResult, expectedModel);
     }
+
+    @Test
+    public void execute_help_add() {
+        CommandResult expectedCommandResult =
+                new CommandResult(AddCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand(AddCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_help_delete() {
+        CommandResult expectedCommandResult =
+                new CommandResult(DeleteCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand(DeleteCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_help_edit() {
+        CommandResult expectedCommandResult =
+                new CommandResult(EditCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand(EditCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_help_find() {
+        CommandResult expectedCommandResult =
+                new CommandResult(FindCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand(FindCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
+    @Test
+    public void execute_help_remark() {
+        CommandResult expectedCommandResult =
+                new CommandResult(RemarkCommand.MESSAGE_USAGE, false, false);
+        assertCommandSuccess(new HelpCommand(RemarkCommand.MESSAGE_USAGE), model, expectedCommandResult, expectedModel);
+    }
+
 }
