e9f9464abdfeab3e7b5bc87729624973f436ab79
Benjamin Tan <benjamin@dev.ofcr.se>
2021-10-26 22:13:23 +0800

    Address PR feedback

--- src/main/java/socialite/model/util/SampleDataUtil.java
+++ src/main/java/socialite/model/util/SampleDataUtil.java
@@ -31,33 +31,34 @@ public class SampleDataUtil {
     public static final Telegram EMPTY_TELEGRAM = new Telegram(null);
     public static final TikTok EMPTY_TIKTOK = new TikTok(null);
     public static final Twitter EMPTY_TWITTER = new Twitter(null);
+    public static final Dates EMPTY_DATES = new Dates();
 
     public static Person[] getSamplePersons() {
         return new Person[] {
             new Person(new Name("Alex Yeoh"), new Phone("87438807"), EMPTY_REMARK,
                 getTagSet("friends"), new Facebook("alex.yeoh"), new Instagram("alex.yeoh"),
                 new Telegram("alyeoh"), new TikTok("alex.yeoh"), EMPTY_TWITTER,
-                new Dates()),
+                EMPTY_DATES),
             new Person(new Name("Bernice Yu"), new Phone("99272758"), EMPTY_REMARK,
                 getTagSet("colleagues", "friends"), new Facebook("bern.yu"),
                 EMPTY_INSTAGRAM, new Telegram("bern_yu"),
-                new TikTok("bernice.yu"), new Twitter("berniceyu"), new Dates()),
+                new TikTok("bernice.yu"), new Twitter("berniceyu"), EMPTY_DATES),
             new Person(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Remark("Her birthday coming!"),
                 getTagSet("neighbours"), EMPTY_FACEBOOK,
                 new Instagram("charlotte.oli"), new Telegram("olichar"),
-                EMPTY_TIKTOK, new Twitter("charolive"), new Dates()),
+                EMPTY_TIKTOK, new Twitter("charolive"), EMPTY_DATES),
             new Person(new Name("David Li"), new Phone("91031282"), EMPTY_REMARK,
                 getTagSet("family"), new Facebook("davey"),
                 new Instagram("davey"), new Telegram("david_li"), new TikTok("david"), new Twitter("david"),
-                new Dates()),
+                EMPTY_DATES),
             new Person(new Name("Irfan Ibrahim"), new Phone("92492021"), new Remark("Outing on Saturday"),
                 getTagSet("classmates"), new Facebook("irfan.him"),
                 new Instagram("irfan.him"), new Telegram("irfan_ibrahim"),
-                new TikTok("irfan"), new Twitter("irfanibrahim"), new Dates()),
+                new TikTok("irfan"), new Twitter("irfanibrahim"), EMPTY_DATES),
             new Person(new Name("Roy Balakrishnan"), new Phone("92624417"), EMPTY_REMARK,
                 getTagSet("colleagues"), new Facebook("balaroy"),
                 new Instagram("balaroy"), EMPTY_TELEGRAM,
-                EMPTY_TIKTOK, new Twitter("royB"), new Dates())
+                EMPTY_TIKTOK, new Twitter("royB"), EMPTY_DATES)
         };
     }
 
