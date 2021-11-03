4ab32c87ec1820b409d20889d6ec85cf958ccbe1
david-eom <david.eom.hyo.shik@gmail.com>
2021-11-04 04:11:41 +0800

    Add test for command history storage

--- src/test/java/socialite/storage/JsonSerializableCommandHistoryTest.java
+++ src/test/java/socialite/storage/JsonSerializableCommandHistoryTest.java
@@ -7,16 +7,13 @@ import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
 
-import socialite.commons.exceptions.IllegalValueException;
 import socialite.commons.util.JsonUtil;
 import socialite.model.CommandHistory;
-import socialite.testutil.Assert;
 import socialite.testutil.TypicalCommandHistory;
 
 public class JsonSerializableCommandHistoryTest {
     private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data", "JsonSerializableCommandHistoryTest");
     private static final Path TYPICAL_COMMAND_HISTORY_FILE = TEST_DATA_FOLDER.resolve("typicalCommandHistory.json");
-    private static final Path INVALID_COMMAND_HISTORY_FILE = TEST_DATA_FOLDER.resolve("invalidCommandHistory.json");
 
     @Test
     public void toModelType_commandHistoryFile_success() throws Exception {
@@ -27,11 +24,4 @@ public class JsonSerializableCommandHistoryTest {
         assertEquals(commandHistoryFromFile, typicalCommandHistory);
     }
 
-
-    @Test
-    public void toModelType_invalidCommandHistoryFile_throwsIllegalValueException() throws Exception {
-        JsonSerializableCommandHistory dataFromFile = JsonUtil.readJsonFile(INVALID_COMMAND_HISTORY_FILE,
-                JsonSerializableCommandHistory.class).get();
-        Assert.assertThrows(IllegalValueException.class, dataFromFile::toModelType);
-    }
 }
